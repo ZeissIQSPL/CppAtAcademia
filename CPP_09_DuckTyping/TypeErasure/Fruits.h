f3206db2a52d403768434f3d4195750a61e87753
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2024-07-31 16:22:25 +0200

    Type erasure sample using C++20 and concepts

---
+++ CPP_09_DuckTyping/TypeErasure/Fruits.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+
+class Apple {
+public:
+    std::string getTypeOfInstanceAsString() const {
+        return "class Apple";
+    }
+};
+
+class Peach {
+public:
+    std::string getTypeOfInstanceAsString() const {
+        return "class Peach";
+    }
+};
